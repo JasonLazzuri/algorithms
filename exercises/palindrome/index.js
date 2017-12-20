cfa58f1e17bc256dab232b7f177a695a303ac05a
JasonLazzuri <jason.Lazzuri@gmail.com>
2017-12-20 11:14:33 -0800

    palindrome, reverse string exercises complete

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,34 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+
+    return str.split("").every((char, i)=>  {
+        return char === str[str.length - i - 1];
+    });
+}
 
 module.exports = palindrome;
+
+
+// function palindrome(str) {
+//     let reversed = '';
+//
+//     for(let i of str){
+//         reversed = i + reversed;
+//     }
+//
+//     if(reversed === str){
+//         return true;
+//     }else{
+//         return false;
+//     }
+//
+// }
+
+// function palindrome(str) {
+// const reversed = str.split("").reverse().join("");
+//
+// return reversed === str;
+//
+// }
