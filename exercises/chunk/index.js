abf847f0cdd566bac382de9a07fc6f34a70f0ff1
JasonLazzuri <jason.Lazzuri@gmail.com>
2018-01-09 14:40:21 -0800

    chunk and anagrams finished

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -9,19 +9,33 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 function chunk(array, size) {
-    const chunked = [];
+  const chunked = [];
+  let index = 0;
 
-    for(let element of array){
-        const last = chunked[chunked.length - 1]
+  while(index < array.length){
+    chunked.push(array.slice(index, index + size));
+    index+= size;
+  }
 
-        if(!last || last.length === size){
-            chunked.push([element]);
-        } else{
-            last.push(element);
-        }
-    }
-
-return chunked;
+    return chunked;
 }
-chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+
 module.exports = chunk;
+
+
+// function chunk(array, size) {
+//     const chunked = [];
+//
+//     for(let element of array){
+//         const last = chunked[chunked.length - 1]
+//
+//         if(!last || last.length === size){
+//             chunked.push([element]);
+//         } else{
+//             last.push(element);
+//         }
+//     }
+//
+//     return chunked;
+//
+// }
